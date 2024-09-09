4640003adc9450a08f3683ce677c16c244025172
Ramzan-Ali12 <m.ramzan.iub.edu@gmail.com>
2024-09-09 23:57:52 +0500

    password reset feature completed

--- users/urls.py
+++ users/urls.py
@@ -17,20 +17,20 @@ urlpatterns = [
     
      # Password reset URLs
     path('password-reset/', PasswordResetView.as_view(
-        template_name='registration/password_reset.html',
-        email_template_name='registration/password_reset_email.html',
-        success_url=reverse_lazy('password_reset_done')  # Use underscores here
+        template_name='users/password_reset.html',
+        html_email_template_name='users/password_reset_email.html',
     ), name='password_reset'),
 
     path('password-reset/done/', PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html'
-    ), name='password_reset_done'),  # Correct name with underscores
+        template_name='users/password_reset_done.html'
+    ), name='password_reset_done'),  
 
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
-        template_name='registration/password_reset_confirm.html'
+        template_name='users/password_reset_confirm.html'
     ), name='password_reset_confirm'),
 
     path('password-reset-complete/', PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html'
+        template_name='users/password_reset_complete.html'
     ), name='password_reset_complete'),
 ]
+
